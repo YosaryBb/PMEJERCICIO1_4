b2fc2474b5b44ab43ec730f73e6f2545ed7c59e3
YosaryBb <98617502+YosaryBb@users.noreply.github.com>
2022-10-26 22:08:34 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pmejercicio1_4/config/Transacciones.java
@@ -0,0 +1,24 @@
+package com.example.pmejercicio1_4.config;
+
+public class Transacciones {
+    public static final String NameDatabase = "DBE4";
+
+    public static final String tablaDatos = "datos";
+
+    public static final String id = "id";
+    public static final String nombre = "nombre";
+    public static final String descripcion = "descripcion";
+    public static final String pathImage = "pathImage";
+    public static final String image = "image";
+
+    public static final String CreateTableDatos = "CREATE TABLE "+tablaDatos+"("+
+            id+" INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            nombre+" TEXT, "+
+            descripcion+" TEXT," +
+            pathImage+" TEXT, "+
+            image+" BLOB)";
+
+    public static final String DropTableDatos = "DROP TABLE IF EXISTS " + tablaDatos;
+
+    public  static final String consultDatos = "SELECT * FROM "+tablaDatos;
+}
